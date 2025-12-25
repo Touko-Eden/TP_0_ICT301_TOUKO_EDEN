42168d1bc9c558dd84bde325ec19431beff097c7
Touko-Eden <toukoeden237@gmail.com>
2025-12-25 16:58:26 +0100

    Update BookBusinessLogic.java

--- SRP/BookBusinessLogic.java
+++ SRP/BookBusinessLogic.java
@@ -1,1 +1,5 @@
-
+public class BookBusinessLogic {
+    public void emprunter(BookSRP book, String lecteur) {
+        System.out.println("Emprunt du livre '" + book.getTitle() + "' par " + lecteur);
+    }
+}
