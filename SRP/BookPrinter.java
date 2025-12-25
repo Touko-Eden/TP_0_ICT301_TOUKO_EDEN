7b1ad14a05eb783d8d913062342ca22d77198a16
Touko-Eden <toukoeden237@gmail.com>
2025-12-25 16:57:28 +0100

    Update BookPrinter.java

--- SRP/BookPrinter.java
+++ SRP/BookPrinter.java
@@ -1,1 +1,8 @@
 
+public class BookPrinter {
+    public void printToScreen(BookSRP book) {
+        System.out.println("Titre: " + book.getTitle());
+        System.out.println("Auteur: " + book.getAuthor());
+        System.out.println("Contenu: " + book.getContent());
+    }
+}
