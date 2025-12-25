499e3caadfc6eeb3bd2bf22b178dc5ef3f2b391e
Touko-Eden <toukoeden237@gmail.com>
2025-12-25 17:19:58 +0100

    Update OrderProcessor_Before.java

--- DIP/OrderProcessor_Before.java
+++ DIP/OrderProcessor_Before.java
@@ -1,1 +1,11 @@
+public class OrderProcessor {
+    private MySQLDatabase database;
 
+    public OrderProcessor() {
+        this.database = new MySQLDatabase();
+    }
+
+    public void processOrder(String order) {
+        database.save(order);
+    }
+}
