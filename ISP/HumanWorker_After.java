5e1d87df993b960b37f532a5bb48341de1a2824e
Touko-Eden <toukoeden237@gmail.com>
2025-12-25 17:13:42 +0100

    Update HumanWorker_After.java

--- ISP/HumanWorker_After.java
+++ ISP/HumanWorker_After.java
@@ -1,1 +1,11 @@
+public class HumanWorker implements Eatable {
+    @Override
+    public void work() {
+        System.out.println("Les humains travaillent");
+    }
 
+    @Override
+    public void eat() {
+        System.out.println("Les humains mangent");
+    }
+}
