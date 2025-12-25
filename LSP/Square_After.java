0f4031f78bbaa5db07dbfc5db50a9acd5e9727f6
Touko-Eden <toukoeden237@gmail.com>
2025-12-25 17:06:44 +0100

    Update Square_After.java

--- LSP/Square_After.java
+++ LSP/Square_After.java
@@ -1,1 +1,13 @@
 
+public class Square implements Shape {
+    private int side;
+
+    public Square(int side) {
+        this.side = side;
+    }
+
+    @Override
+    public int getArea() {
+        return side * side;
+    }
+}
